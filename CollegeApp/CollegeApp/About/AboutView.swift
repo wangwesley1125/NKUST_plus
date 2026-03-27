cd694586e59e43d31acb1632bc0e0e83726ac28e
WesleyWang <wangwesley1125@gmail.com>
2026-03-27 21:03:33 +0800

    Add about nkust link in AboutView

--- CollegeApp/CollegeApp/About/AboutView.swift
+++ CollegeApp/CollegeApp/About/AboutView.swift
@@ -29,7 +29,7 @@ struct AboutView: View {
                         HStack {
                             Text("版本資訊")
                             Spacer()
-                            Text("1.1.0")
+                            Text("1.2.0")
                         }
                     }
                     .foregroundColor(.teal)
@@ -73,6 +73,72 @@ struct AboutView: View {
                                 .foregroundColor(.teal)
                             }
                     }
+                    
+                    Section {
+                        Button {
+                            let appURL = URL(string: "https://apps.apple.com/tw/app/%E9%AB%98%E7%A7%91-plus/id6760967835")!
+                            let activityVC = UIActivityViewController(
+                                activityItems: ["推薦你使用 NKUST plus！", appURL],
+                                applicationActivities: nil
+                            )
+                            
+                            // 取得目前的 UIWindow 來呈現
+                            if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+                               let window = windowScene.windows.first {
+                                window.rootViewController?.present(activityVC, animated: true)
+                            }
+                        } label: {
+                            HStack {
+                                Text("分享此 App")
+                            }
+                            .foregroundColor(.teal)
+                        }
+                    }
+                }
+                
+                // 關於高科大
+                Section("關於高雄科技大學") {
+                    
+                    Link(destination: URL(string: "https://www.nkust.edu.tw/")!) {
+                        Text("國立高雄科技大學官網")
+                    }
+                    .foregroundColor(.teal)
+                    
+                    Link(destination: URL(string: "https://acad.nkust.edu.tw/")!) {
+                        Text("教務處")
+                    }
+                    .foregroundColor(.teal)
+                    
+                    Link(destination: URL(string: "https://osa.nkust.edu.tw/")!) {
+                        Text("學務處")
+                    }
+                    .foregroundColor(.teal)
+                    
+                    Link(destination: URL(string: "https://gen.nkust.edu.tw/")!) {
+                        Text("總務處")
+                    }
+                    .foregroundColor(.teal)
+                    
+                    Link(destination: URL(string: "https://oia.nkust.edu.tw/")!) {
+                        Text("國際事務處")
+                    }
+                    .foregroundColor(.teal)
+                    
+                    Link(destination: URL(string: "https://oosaf.nkust.edu.tw/")!) {
+                        Text("學務資訊系統（請假 / 曠課 / 講懲 / 幹部...）")
+                    }
+                    .foregroundColor(.teal)
+                    
+                    Link(destination: URL(string: "https://www.lib.nkust.edu.tw/portal/")!) {
+                        Text("國立高雄科技大學 圖書館")
+                    }
+                    .foregroundColor(.teal)
+                    
+                    
+                    Link(destination: URL(string: "https://www.nkust.edu.tw/p/412-1000-85.php")!) {
+                        Text("其他行政單位")
+                    }
+                    .foregroundColor(.teal)
                     
                 }
                 
